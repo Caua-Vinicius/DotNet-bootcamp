04c40882c06f564a1d839f10fdf49d52b70ce82b
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2024-05-12 23:11:45 -0300

    Adicionando desafio de projeto Estacionamento e remoção de arquivos inuteis para github

--- AulasPraticas/Aula_2_Arrays/obj/Debug/net8.0/Aula_2_Arrays.AssemblyInfo.cs
+++ AulasPraticas/Aula_2_Arrays/obj/Debug/net8.0/Aula_2_Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula_2_Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26d0293c715c5b9260ed77eead44726879b603c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9f4a1a5fda7531e81bbf77e619c7bd6e7e9132e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula_2_Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula_2_Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
